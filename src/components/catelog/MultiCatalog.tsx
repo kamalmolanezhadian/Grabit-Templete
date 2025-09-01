02c5331926f3cbefcfb98813d388fdcb6924ca44
Hotel <121792616+kamalmolanezhadian@users.noreply.github.com>
2025-09-01 16:19:00 +0330

    Update MultiCatalog.tsx

--- src/components/catelog/MultiCatalog.tsx
+++ src/components/catelog/MultiCatalog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -5,49 +7,24 @@ import ItemCard, { ItemCardProps } from "../itemCard/ItemCard";
 
 interface MultiCatalogProps {
   data: any[];
+  handleWishlist: (item: any) => void;
 }
 
-const MultiCatalog: React.FC<MultiCatalogProps> = ({ data }) => {
-  const [catalogData, setCatalogData] = useState<any[]>([]);
+const MultiCatalog: React.FC<MultiCatalogProps> = ({ data, handleWishlist }) => {
+  const [items, setItems] = useState<any[]>([]);
 
   useEffect(() => {
-    setCatalogData(data);
+    setItems(data);
   }, [data]);
 
-  const handleWishlist = (item: any) => {
-    console.log("Added to wishlist:", item);
-    // اینجا کد واقعی اضافه کردن به علاقه‌مندی‌ها را بنویسید
-  };
-
-  const getData = () => {
-    return catalogData;
-  };
-
-  const swiperParams = {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    loop: true,
-    breakpoints: {
-      320: { slidesPerView: 1 },
-      480: { slidesPerView: 2 },
-      768: { slidesPerView: 3 },
-      1024: { slidesPerView: 4 },
-    },
-  };
-
   return (
-    <div className="multi-catalog">
-      <Swiper {...swiperParams}>
-        {getData().map((item: any, index: number) => (
-          <SwiperSlide key={index}>
-            <ItemCard 
-              data={item} 
-              handleWishlist={() => handleWishlist(item)} 
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <Swiper spaceBetween={20} slidesPerView={4}>
+      {items.map((item, index) => (
+        <SwiperSlide key={index}>
+          <ItemCard data={item} handleWishlist={handleWishlist} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
